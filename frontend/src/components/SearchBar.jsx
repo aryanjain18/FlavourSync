4ac3ea91a5a3d5d1cc258cced11a46be3c3983c0
Swatiiisinghh <swatisinghhh211@gmail.com>
2024-11-15 21:35:03 +0530

    GoodChanges

--- frontend/src/components/SearchBar.jsx
+++ frontend/src/components/SearchBar.jsx
@@ -1,10 +1,21 @@
 // src/components/SearchBar.js
+import React, { useEffect } from 'react';
 import './SearchBar.css';
 import { FaSearch } from 'react-icons/fa';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 function SearchBar() {
+  // Initialize AOS
+  useEffect(() => {
+    AOS.init({ duration: 1000 }); // Animation duration in ms
+  }, []);
+
   return (
-    <div className="search-bar">
+    <div 
+      className="search-bar"
+      data-aos="fade-up" // Specify the type of animation
+    >
       <input
         type="text"
         placeholder="Search flavors, cuisines, moods..."
@@ -18,3 +29,7 @@ function SearchBar() {
 }
 
 export default SearchBar;
+AOS.init({
+  duration: 1200, // 1.2 seconds
+  offset: 50,     // Trigger animation 50px before the element is visible
+});
